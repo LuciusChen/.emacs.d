7956277017cc42c99e7c308c2f3b095f2635d771
Lucius Chen <luciuschen@Luciuss-MacBook-Pro.local>
2023-04-22 11:17:28 +0800

    wrap with setup

---
+++ lib/lib-window.el
@@ -0,0 +1,22 @@
+;;; lib-window.el --- window setup -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+(defun split-window-horizontally-instead ()
+  "Kill any other windows and re-split such that the current window is on the top half of the frame."
+  (interactive)
+  (let ((other-buffer (and (next-window) (window-buffer (next-window)))))
+    (delete-other-windows)
+    (split-window-horizontally)
+    (when other-buffer
+      (set-window-buffer (next-window) other-buffer))))
+
+(defun split-window-vertically-instead ()
+  "Kill any other windows and re-split such that the current window is on the left half of the frame."
+  (interactive)
+  (let ((other-buffer (and (next-window) (window-buffer (next-window)))))
+    (delete-other-windows)
+    (split-window-vertically)
+    (when other-buffer
+      (set-window-buffer (next-window) other-buffer))))
+(provide 'lib-window)
+;;; lib-window.el ends here
