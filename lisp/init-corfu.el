0a7a651f4dabbb7f94abf816ff3ce1f5eb74fd7f
Lucius Chen <luciuschen@Luciuss-MacBook-Pro.local>
2023-04-22 13:07:57 +0800

    adjust indent

--- lisp/init-corfu.el
+++ lisp/init-corfu.el
@@ -6,21 +6,24 @@
   (:when-loaded
     ;; pinyinlib.el 用于匹配简体/繁体汉字拼音首字母
     (add-to-list 'orderless-matching-styles
-                 (lambda (str) (orderless-regexp
-                                (pinyinlib-build-regexp-string str))))))
+                 (lambda (str)
+                   (orderless-regexp
+                    (pinyinlib-build-regexp-string str))))))
 
 (setup kind-icon
   (:load-after corfu)
   (:when-loaded
-    (add-to-list 'corfu-margin-formatters #'kind-icon-margin-formatter)))
+    (add-to-list 'corfu-margin-formatters
+                 #'kind-icon-margin-formatter)))
 
 (setup corfu
   ;; org-mode 中关闭补全
   (:option corfu-excluded-modes '(org-mode))
   (global-corfu-mode)
   (:when-loaded
-    (:with-mode eshell-mode (:hook (lambda () (setq-local corfu-auto nil)))
-                (setq-default corfu-auto t)
-                (setq-default corfu-quit-no-match 'separator))))
+    (:with-mode eshell-mode
+      (:hook (lambda () (setq-local corfu-auto nil)))
+      (setq-default corfu-auto t)
+      (setq-default corfu-quit-no-match 'separator))))
 (provide 'init-corfu)
 ;;; init-corfu.el ends here
