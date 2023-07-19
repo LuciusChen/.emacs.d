8f5597ddc3dcee4ffdadf25283e0d7df17bf9179
Lucius Chen <luciuschen@Luciuss-MacBook-Pro.local>
2023-07-19 16:21:51 +0800

    remove custom

--- init.el
+++ init.el
@@ -178,21 +178,6 @@
 (require 'init-latex)
 (require 'init-local)
 (provide 'init)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(ef-spring))
- '(elfeed-feeds
-   '("https://karthinks.com/index.xml" "https://blog.jcole.us/feed/" "https://kisaragi-hiu.com/feed.xml")))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
 ;; Local Variables:
 ;; coding: utf-8
 ;; no-byte-compile: t
