6e8b66c6e787452dc02dd2876caca16664c4b556
lucius <chenyaohua@njcjh.cn>
2023-10-16 16:16:03 +0800

    add some defun and add notification to tab-bar

--- lib/lib-telega.el
+++ lib/lib-telega.el
@@ -43,13 +43,55 @@
 (defun lucius/telega-chat-mode ()
   (telega-add-company-backends))
 
+(defun lucius/telega-save-file-to-clipboard (msg)
+  "Save file at point to clipboard.
+NOTE: macOS only."
+  (interactive (list (telega-msg-for-interactive)))
+  (let ((file (telega-msg--content-file msg)))
+    (unless file
+      (user-error "No file associated with message"))
+    (telega-file--download file 32
+      (lambda (dfile)
+        (telega-msg-redisplay msg)
+        (message "Wait for downloading to finish…")
+        (when (telega-file--downloaded-p dfile)
+          (let* ((fpath (telega--tl-get dfile :local :path)))
+            (shell-command (format "osascript -e 'set the clipboard to POSIX file \"%s\"'" fpath))
+            (message (format "File saved to clipboard: %s" fpath))))))))
+
+(defun lucius/telega-msg-save-to-cloud-copyleft (msg)
+  "Save messages's MSG media content to a file.
+     If MSG is an animation message, then possibly add animation to
+     the saved animations list."
+  (interactive (list (telega-msg-for-interactive)))
+  (let ((file (telega-msg--content-file msg)))
+    (unless file
+      (user-error "No file associated with message"))
+    (telega-file--download file 32
+      (lambda (dfile)
+        (telega-msg-redisplay msg)
+        (when (telega-file--downloaded-p dfile)
+          ;; TODO: This might be executed in process filter, so
+          ;; pressing C-g will trigger "error in process filter: Quit"
+          ;; Need to execute this outside of process filter
+          (let* ((fpath (telega--tl-get dfile :local :path))
+                 (fname (file-name-nondirectory fpath)))
+            (telega--sendMessage
+             (telega-chat-me)
+             (list :@type "inputMessageDocument"
+                   :document (telega-chatbuf--gen-input-file
+                              fpath 'Document)
+                   :caption (telega-fmt-text "#copyleft")
+                   :disable_content_type_detection nil))
+            (message (format "Saved to cloud: %s" fname))))))))
+
 (defun lg-telega-root-mode ()
   (hl-line-mode 1))
 
 (defun lg-telega-chat-update (chat)
   (with-telega-root-buffer
     (hl-line-highlight)))
-
+;; reply and forward
 (defmacro lucius/telega-ins--aux-inline-reply (&rest body)
   `(telega-ins--aux-inline
        "➦" 'telega-msg-inline-reply
