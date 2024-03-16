2742807bb02e60ffaad2d83752887531f5fef207
Lucius Chen <luciuschen@Luciuss-MacBook-Pro.local>
2024-03-16 14:17:11 +0800

    remove ibuffer-vc and nerd-icons-ibuffer

--- lisp/init-layout.el
+++ lisp/init-layout.el
@@ -5,42 +5,6 @@
 ;;   See http://www.reddit.com/r/emacs/comments/21fjpn/fontifying_buffer_list_for_emacs_243/
 
 ;;; Code:
-(setup ibuffer
-  (:global "C-x C-b" ibuffer)
-  (:option ibuffer-formats
-           '((mark modified read-only vc-status-mini " "
-              (name 22 22 :left :elide)
-              " "
-              (size-h 9 -1 :right)
-              " "
-              (mode 12 12 :left :elide)
-              " "
-              vc-relative-file)
-             (mark modified read-only vc-status-mini " "
-              (name 22 22 :left :elide)
-              " "
-              (size-h 9 -1 :right)
-              " "
-              (mode 14 14 :left :elide)
-              " "
-              (vc-status 12 12 :left)
-              " "
-              vc-relative-file))
-           ibuffer-filter-group-name-face 'font-lock-doc-face)
-  (:when-loaded
-    (:require fullframe)
-    (fullframe ibuffer ibuffer-quit)
-    (:require nerd-icons-ibuffer)
-    (:hooks ibuffer-hook (lambda () (ibuffer-vc-set-filter-groups-by-vc-root)
-                           (unless (eq ibuffer-sorting-mode 'filename/process)
-                             (ibuffer-do-sort-by-filename/process)))
-            ibuffer-mode-hook nerd-icons-ibuffer-mode)
-    (setq-default ibuffer-show-empty-filter-groups nil)
-    ;; Use human readable Size column instead of original one
-    (define-ibuffer-column size-h
-        (:name "Size" :inline t)
-      (file-size-human-readable (buffer-size)))))
-
 (setup popper
   (:global "C-~"   popper-toggle
            "M-~"   popper-cycle
