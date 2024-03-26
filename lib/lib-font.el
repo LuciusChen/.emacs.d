c48c1b9f3b1093b2fc1024040fb68b03c635917f
Lucius Chen <luciuschen@Luciuss-MacBook-Pro.local>
2024-03-26 15:01:51 +0800

    add jetbrain ligatures

--- lib/lib-font.el
+++ lib/lib-font.el
@@ -5,9 +5,7 @@
   ;; https://typeof.net/Iosevka/customizer
   ;; https://github.com/be5invis/Iosevka/blob/v21.0.0/doc/PACKAGE-LIST.md
   ;; Setting the default
-  ;; (set-face-attribute 'default nil :font "IBM Plex Mono 14" :weight 'normal)
-  (set-face-attribute 'default nil :font "Berkeley Mono 14" :weight 'normal)
-  ;; (set-face-attribute 'default nil :font "Fira Code 14" :weight 'normal)
+  (set-face-attribute 'default nil :font "JetBrains Mono 14" :weight 'light)
   ;; (set-face-attribute 'default nil :font "Iosevka Lucius 14" :weight 'normal)
   ;; 特殊字符需要安装 Symbola 字体
   ;; https://www.wfonts.com/font/symbola
@@ -45,9 +43,34 @@
   (set-fontset-font t 'han (font-spec :script 'han) nil 'append)
   ;; IBM Plex Mono 没有这几个字符，自己编辑字体后添加，可以省去下面的设置。
   (set-fontset-font nil ?❤ "Iosevka Lucius")
-  (set-fontset-font nil ?☑ "Iosevka Lucius")
-  (set-fontset-font nil ?☐ "Iosevka Lucius")
-  (set-fontset-font nil ?⦚ "Iosevka Lucius")
+  ;; (set-fontset-font nil ?☑ "Iosevka Lucius")
+  ;; (set-fontset-font nil ?☐ "Iosevka Lucius")
   (set-fontset-font t 'javanese "Noto Sans Javanese"))
+
+(defconst jetbrains-ligature-mode--ligatures
+  '("-->" "//" "/**" "/*" "*/" "<!--" ":=" "->>" "<<-" "->" "<-"
+    "<=>" "==" "!=" "<=" ">=" "=:=" "!==" "&&" "||" "..." ".."
+    "|||" "///" "&&&" "===" "++" "--" "=>" "|>" "<|" "||>" "<||"
+    "|||>" "<|||" ">>" "<<" "::=" "|]" "[|" "{|" "|}"
+    "[<" ">]" ":?>" ":?" "/=" "[||]" "!!" "?:" "?." "::"
+    "+++" "??" "###" "##" ":::" "####" ".?" "?=" "=!=" "<|>"
+    "<:" ":<" ":>" ">:" "<>" "***" ";;" "/==" ".=" ".-" "__"
+    "=/=" "<-<" "<<<" ">>>" "<=<" "<<=" "<==" "<==>" "==>" "=>>"
+    ">=>" ">>=" ">>-" ">-" "<~>" "-<" "-<<" "=<<" "---" "<-|"
+    "<=|" "/\\" "\\/" "|=>" "|~>" "<~~" "<~" "~~" "~~>" "~>"
+    "<$>" "<$" "$>" "<+>" "<+" "+>" "<*>" "<*" "*>" "</>" "</" "/>"
+    "<->" "..<" "~=" "~-" "-~" "~@" "^=" "-|" "_|_" "|-" "||-"
+    "|=" "||=" "#{" "#[" "]#" "#(" "#?" "#_" "#_(" "#:" "#!" "#="
+    "&="))
+
+(sort jetbrains-ligature-mode--ligatures (lambda (x y) (> (length x) (length y))))
+
+(dolist (pat jetbrains-ligature-mode--ligatures)
+  (set-char-table-range composition-function-table
+                        (aref pat 0)
+                        (nconc (char-table-range composition-function-table (aref pat 0))
+                               (list (vector (regexp-quote pat)
+                                             0
+                                             'compose-gstring-for-graphic)))))
 (provide 'lib-font)
 ;;; lib-font.el ends here
