88f8359813ac7829d0d6a6aea09817a36d47cca5
Lucius Chen <luciuschen@192.168.2.14>
2024-03-31 22:18:49 +0800

    add custom magit UI

--- lisp/init-vc.el
+++ lisp/init-vc.el
@@ -15,14 +15,32 @@
     (:global [(meta f12)] magit-status
              "C-x g" magit-status
              "C-x M-g" magit-dispatch)
-    (:option magit-diff-refine-hunk t)
+    (:option magit-diff-refine-hunk t
+             ;; Don't autosave repo buffers. This is too magical, and saving can
+             ;; trigger a bunch of unwanted side-effects, like save hooks and
+             ;; formatters. Trust the user to know what they're doing.
+             magit-save-repository-buffers nil
+             ;; Don't display parent/related refs in commit buffers; they are rarely
+             ;; helpful and only add to runtime costs.
+             magit-revision-insert-related-refs nil)
     (:advice magit-status :around #'magit-fullscreen)
     (:advice magit-mode-quit-window :after #'magit-restore-screen)
     ;; kill 因为 blob-next 和 blob-previous 产生的 buffer
     (:advice magit-blob-next :around #'kill-all-blob-next-after-quit)
     (:advice magit-blob-previous :around #'kill-all-blob-previous-after-quit)
     (when *IS-MAC*
-      (add-hook 'magit-mode-hook (lambda () (local-unset-key [(meta h)]))))))
+      (add-hook 'magit-mode-hook (lambda () (local-unset-key [(meta h)]))))
+
+    (:with-feature magit-log
+      ;; Set `magit-log-margin' value in :init as many other variables will be
+      ;; dynamically set based on its value when `magit-log' is loaded.
+      ;; (setq magit-log-margin '(t age magit-log-margin-width t 18)) ;Default value
+      ;; Show the commit ages with 1-char time units
+      ;;   minute->m, hour->h, day->d, week->w, month->M, year->Y
+      ;; Also reduce the author column width to 11 as the author name is being
+      ;; abbreviated below.
+      (:option magit-log-margin '(t age-abbreviated magit-log-margin-width :author 11))
+      (advice-add 'magit-log-format-margin :filter-args #'modi/magit-log--abbreviate-author))))
 
 (setup forge
   (:load-after magit)
