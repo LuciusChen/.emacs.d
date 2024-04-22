2359692914a17210ad2ea61ccc76a3e2f6bca172
Lucius Chen <luciuschen@Luciuss-MacBook-Pro.local>
2024-04-22 09:36:29 +0800

    remove welcome prefix

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -14,18 +14,18 @@
 ;; Change global font size easily
 (setup default-text-scale (:hook-into after-init))
 
-(setup welcome-dashboard
+(setup dashboard
   (:after nerd-icons)
-  (:option welcome-dashboard-latitude 32.088258
-           welcome-dashboard-longitude 118.822916 ;; latitude and longitude must be set to show weather information
-           welcome-dashboard-path-max-length 75
-           welcome-dashboard-use-fahrenheit nil ;; show in celcius or fahrenheit.
-           welcome-dashboard-min-left-padding 10
-           welcome-dashboard-image-file "~/.emacs.d/assets/bitmap.png"
-           welcome-dashboard-image-width 400
-           welcome-dashboard-image-height 169
-           welcome-dashboard-title "Happy hacking, lucius - Emacs ❤ you")
-  (welcome-dashboard-create-welcome-hook))
+  (:option dashboard-latitude 32.088258
+           dashboard-longitude 118.822916 ;; latitude and longitude must be set to show weather information
+           dashboard-path-max-length 75
+           dashboard-use-fahrenheit nil ;; show in celcius or fahrenheit.
+           dashboard-min-left-padding 10
+           dashboard-image-file "~/.emacs.d/assets/bitmap.png"
+           dashboard-image-width 400
+           dashboard-image-height 169
+           dashboard-title "Happy hacking, lucius - Emacs ❤ you")
+  (dashboard-create-hook))
 
 (setup custom
   (:when-loaded
