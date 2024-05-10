e347baab13d6d8224f436ee52c38e821b302d9cd
Lucius Chen <luciuschen@Luciuss-MacBook-Pro.local>
2024-05-10 15:16:39 +0800

    update

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -412,9 +412,45 @@
                                                      (?S . ebib-create-org-stamp-inactive))
              ebib-preload-bib-files bibtex-files
              ebib-use-timestamp t)))
-
 (setup citar
   (:load-after org)
+  ;; nerd-icons
+  (defvar citar-indicator-files
+    (citar-indicator-create
+     :symbol (nerd-icons-faicon
+              "nf-fa-file_o"
+              :face 'nerd-icons-green
+              :v-adjust -0.1)
+     :function #'citar-has-files
+     :padding "  " ; need this because the default padding is too low for these icons
+     :tag "has:files"))
+  (defvar citar-indicator-links
+    (citar-indicator-create
+     :symbol (nerd-icons-faicon
+              "nf-fa-link"
+              :face 'nerd-icons-orange
+              :v-adjust 0.01)
+     :function #'citar-has-links
+     :padding "  "
+     :tag "has:links"))
+  (defvar citar-indicator-notes
+    (citar-indicator-create
+     :symbol (nerd-icons-codicon
+              "nf-cod-note"
+              :face 'nerd-icons-blue
+              :v-adjust -0.3)
+     :function #'citar-has-notes
+     :padding "    "
+     :tag "has:notes"))
+  (defvar citar-indicator-cited
+    (citar-indicator-create
+     :symbol (nerd-icons-faicon
+              "nf-fa-circle_o"
+              :face 'nerd-icon-green)
+     :function #'citar-is-cited
+     :padding "  "
+     :tag "is:cited"))
+
   (:when-loaded
     (:option org-cite-global-bibliography '("~/Library/CloudStorage/Dropbox/org/bib/bibtex.bib")
              citar-notes-paths (list "~/Library/CloudStorage/Dropbox/org/main")
@@ -422,43 +458,7 @@
              org-cite-insert-processor 'citar
              org-cite-follow-processor 'citar
              org-cite-activate-processor 'citar
-             citar-bibliography org-cite-global-bibliography)
-
-    (defvar citar-indicator-files
-      (citar-indicator-create
-       :symbol (nerd-icons-faicon
-                "nf-fa-file_o"
-                :face 'nerd-icons-green
-                :v-adjust -0.1)
-       :function #'citar-has-files
-       :padding "  " ; need this because the default padding is too low for these icons
-       :tag "has:files"))
-    (defvar citar-indicator-links
-      (citar-indicator-create
-       :symbol (nerd-icons-faicon
-                "nf-fa-link"
-                :face 'nerd-icons-orange
-                :v-adjust 0.01)
-       :function #'citar-has-links
-       :padding "  "
-       :tag "has:links"))
-    (defvar citar-indicator-notes
-      (citar-indicator-create
-       :symbol (nerd-icons-codicon
-                "nf-cod-note"
-                :face 'nerd-icons-blue
-                :v-adjust -0.3)
-       :function #'citar-has-notes
-       :padding "    "
-       :tag "has:notes"))
-    (defvar citar-indicator-cited
-      (citar-indicator-create
-       :symbol (nerd-icons-faicon
-                "nf-fa-circle_o"
-                :face 'nerd-icon-green)
-       :function #'citar-is-cited
-       :padding "  "
-       :tag "is:cited"))))
+             citar-bibliography org-cite-global-bibliography)))
 
 (setup org-modern
   (:load-after org)
