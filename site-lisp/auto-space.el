632c33158231091e5b1003fabfacd9095cac3724
Lucius Chen <luciuschen@Luciuss-MacBook-Pro.local>
2024-06-05 20:25:56 +0800

    update

--- site-lisp/auto-space.el
+++ site-lisp/auto-space.el
@@ -31,7 +31,10 @@
 
 (defun is-halfwidth-character (char)
   "Determine if a character is a halfwidth character using char-width."
-  (and char (not (member char '(?\s ?~ ?“ ?” ?# ?( ?) ?[ ?] ?{ ?} ?' ?\"))) (= (char-width char) 1)))
+  (and char (or (and (>= char ?a) (<= char ?z))
+                (and (>= char ?A) (<= char ?Z))
+                (and (>= char ?0) (<= char ?9))
+                (and char (member char '(?%))))))
 
 (defun should-insert-space (char1 char2)
   "Determine if a space should be inserted between CHAR1 and CHAR2."
