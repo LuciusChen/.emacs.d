88048eb139b388d0c92bd3c9ccde3dac1aa0f36d
Lucius Chen <luciuschen@Luciuss-MacBook-Pro.local>
2024-09-18 15:13:13 +0800

    refactor: consolidate macOS-specific configurations into main init files
    - Remove straight.el installation from early-init.el
    - Add macOS-specific configurations directly to init.el and init-minimum.el
    - Delete unnecessary init-mac.el file

--- init.el
+++ init.el
@@ -22,22 +22,74 @@
 (defconst *IS-LINUX* (memq system-type '(gnu gnu/linux gnu/kfreebsd berkeley-unix)))
 (defconst *org-path* "~/Library/CloudStorage/Dropbox/org")
 
+(when *IS-MAC*
+  ;; modify meta from ⌥ to ⌘
+  (setq mac-command-modifier 'meta)
+  (setq mac-option-modifier 'super)
+  ;; Make mouse wheel / trackpad scrolling less jerky
+  (setq mouse-wheel-scroll-amount '(1 ((shift) . 5) ((control))))
+  (dolist (multiple '("" "double-" "triple-"))
+    (dolist (direction '("right" "left"))
+      (global-set-key (read-kbd-macro (concat "<" multiple "wheel-" direction ">")) 'ignore)))
+  (global-set-key (kbd "M-`") 'ns-next-frame)
+
+  (defconst +env-file (concat user-emacs-directory ".env"))
+
+  (defun +load-env-file (file &optional noerror)
+    "Read and set envvars from FILE.
+If NOERROR is non-nil, don't throw an error if the file doesn't exist or is
+unreadable. Returns the names of envvars that were changed."
+    (if (not (file-readable-p file))
+        (unless noerror
+          (signal 'file-error (list "Couldn't read envvar file" file)))
+      (let (envvars environment)
+        (with-temp-buffer
+          (save-excursion
+            (insert "\n")
+            (insert-file-contents file))
+          (while (re-search-forward "\n *\\([^#= \n]*\\)=" nil t)
+            (push (match-string 1) envvars)
+            (push (buffer-substring
+                   (match-beginning 1)
+                   (1- (or (save-excursion
+                             (when (re-search-forward "^\\([^= ]+\\)=" nil t)
+                               (line-beginning-position)))
+                           (point-max))))
+                  environment)))
+        (when environment
+          (setq process-environment
+                (append (nreverse environment) process-environment)
+                exec-path
+                (if (member "PATH" envvars)
+                    (append (split-string (getenv "PATH") path-separator t)
+                            (list exec-directory))
+                  exec-path)
+                shell-file-name
+                (if (member "SHELL" envvars)
+                    (or (getenv "SHELL") shell-file-name)
+                  shell-file-name))
+          envvars))))
+
+  (when (and (or (display-graphic-p))
+             (file-exists-p +env-file))
+    (+load-env-file +env-file)))
+
 ;; Install straight.el
 ;; branch develop
-;; (setq straight-repository-branch "develop")
-;; (setq straight-check-for-modifications '(check-on-save find-when-checking))
-;; (defvar bootstrap-version)
-;; (let ((bootstrap-file
-;;        (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-;;       (bootstrap-version 6))
-;;   (unless (file-exists-p bootstrap-file)
-;;     (with-current-buffer
-;;         (url-retrieve-synchronously
-;;          "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
-;;          'silent 'inhibit-cookies)
-;;       (goto-char (point-max))
-;;       (eval-print-last-sexp)))
-;;   (load bootstrap-file nil 'nomessage))
+(setq straight-repository-branch "develop")
+(setq straight-check-for-modifications '(check-on-save find-when-checking))
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
 ;; install packages
 (defvar *use-package-list*
@@ -182,7 +234,6 @@
 (require 'init-setup)
 (require 'init-builtin)
 (require 'init-ui)
-(when *IS-MAC* (require 'init-mac))
 
 (require 'init-editing)
 (require 'init-vc)
