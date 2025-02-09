3569016f1a424ebc583c1582f6f370c6cf6ae609
Lucius <chenyh572@gmai.com>
2025-02-09 14:26:52 +0800

    feat(lib-org): add +eww-to-org function for rendering eww buffer as org markup
    refactor(init-local): remove jao-eww-to-org function and add my-open-Marked function for opening files in Marked 2

--- lib/lib-org.el
+++ lib/lib-org.el
@@ -1,4 +1,7 @@
 ;; lib-org.el --- Initialize org	-*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
+
 ;; Save the corresponding buffers
 (defun log-todo-next-creation-date (&rest ignore)
   "Log NEXT creation time in the property drawer under the key 'ACTIVATED'"
@@ -95,6 +98,49 @@ See also `org-save-all-org-buffers'"
   (interactive)
   (call-interactively 'org-latex-preview-clear-cache)
   (org-latex-preview 'buffer))
+
+(defun +eww-to-org (&optional dest)
+  "Render the current eww buffer using org markup.
+If DEST, a buffer, is provided, insert the markup there."
+  (interactive)
+  (unless (org-region-active-p)
+    (let ((shr-width 80)) (eww-readable)))
+  (let* ((start (if (org-region-active-p) (region-beginning) (point-min)))
+         (end (if (org-region-active-p) (region-end) (point-max)))
+         (buff (or dest (generate-new-buffer "*eww-to-org*")))
+         (link (eww-current-url))
+         (title (or (plist-get eww-data :title) "")))
+    (with-current-buffer buff
+      (insert "#+title: " title "\n#+link: " link "\n\n")
+      (org-mode))
+    (save-excursion
+      (goto-char start)
+      (while (< (point) end)
+        (let* ((p (point))
+               (props (text-properties-at p))
+               (k (seq-find (lambda (x) (plist-get props x))
+                            '(shr-url image-url outline-level face)))
+               (prop (and k (list k (plist-get props k))))
+               (next (if prop
+                         (next-single-property-change p (car prop) nil end)
+                       (next-property-change p nil end)))
+               (txt (buffer-substring (point) next))
+               (txt (replace-regexp-in-string "\\*" "·" txt)))
+          (with-current-buffer buff
+            (insert
+             (pcase prop
+               ((and (or `(shr-url ,url) `(image-url ,url))
+                     (guard (string-match-p "^http" url)))
+                (let ((tt (replace-regexp-in-string "\n\\([^$]\\)" " \\1" txt)))
+                  (org-link-make-string url tt)))
+               (`(outline-level ,n)
+                (concat (make-string (- (* 2 n) 1) ?*) " " txt "\n"))
+               ('(face italic) (format "/%s/ " (string-trim txt)))
+               ('(face bold) (format "*%s* " (string-trim txt)))
+               (_ txt))))
+          (goto-char next))))
+    (pop-to-buffer buff)
+    (goto-char (point-min))))
 (provide 'lib-org)
 ;;;; provide
 ;;; lib-org.el ends here.
--- lisp/init-local.el
+++ lisp/init-local.el
@@ -2,49 +2,6 @@
 ;;; Commentary:
 ;;; Code:
 
-(defun jao-eww-to-org (&optional dest)
-  "Render the current eww buffer using org markup.
-If DEST, a buffer, is provided, insert the markup there."
-  (interactive)
-  (unless (org-region-active-p)
-    (let ((shr-width 80)) (eww-readable)))
-  (let* ((start (if (org-region-active-p) (region-beginning) (point-min)))
-         (end (if (org-region-active-p) (region-end) (point-max)))
-         (buff (or dest (generate-new-buffer "*eww-to-org*")))
-         (link (eww-current-url))
-         (title (or (plist-get eww-data :title) "")))
-    (with-current-buffer buff
-      (insert "#+title: " title "\n#+link: " link "\n\n")
-      (org-mode))
-    (save-excursion
-      (goto-char start)
-      (while (< (point) end)
-        (let* ((p (point))
-               (props (text-properties-at p))
-               (k (seq-find (lambda (x) (plist-get props x))
-                            '(shr-url image-url outline-level face)))
-               (prop (and k (list k (plist-get props k))))
-               (next (if prop
-                         (next-single-property-change p (car prop) nil end)
-                       (next-property-change p nil end)))
-               (txt (buffer-substring (point) next))
-               (txt (replace-regexp-in-string "\\*" "·" txt)))
-          (with-current-buffer buff
-            (insert
-             (pcase prop
-               ((and (or `(shr-url ,url) `(image-url ,url))
-                     (guard (string-match-p "^http" url)))
-                (let ((tt (replace-regexp-in-string "\n\\([^$]\\)" " \\1" txt)))
-                  (org-link-make-string url tt)))
-               (`(outline-level ,n)
-                (concat (make-string (- (* 2 n) 1) ?*) " " txt "\n"))
-               ('(face italic) (format "/%s/ " (string-trim txt)))
-               ('(face bold) (format "*%s* " (string-trim txt)))
-               (_ txt))))
-          (goto-char next))))
-    (pop-to-buffer buff)
-    (goto-char (point-min))))
-
 ;; pipx install aider
 (setup aider
   (:load-after password-store)
@@ -74,5 +31,11 @@ If DEST, a buffer, is provided, insert the markup there."
   (set-buffer-modified-p t)
   (save-buffer))
 
+(defun my/my-open-Marked ()
+  "Open the current file in Marked 2."
+  (interactive)
+  (if (not buffer-file-name)
+      (error "Must be visiting a file")
+    (call-process-shell-command (format "open -a \"Marked 2\" \"%s\"" buffer-file-name))))
 (provide 'init-local)
 ;;; init-local.el ends here
