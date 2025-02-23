c8d84cd831a56342c0619b69095bddba5cb9eb43
Lucius <chenyh572@gmai.com>
2025-02-23 10:00:26 +0800

    refactor: update org and ui setup for embark and faces
    Refactored the org setup to use :after instead of :with-feature for embark integration. Added a new :after setup-define for better feature loading. Updated ui setup to use 'faces' instead of 'font' for font loading.

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -389,10 +389,9 @@
     ;; 解决 org-roam-node-find 时，内容局限于 buffer 宽度。
     (:advice org-roam-node-read--to-candidate :override +org-roam-node-read--to-candidate)
     (org-roam-db-autosync-enable)
-    (:with-feature embark
-      (:when-loaded
-        (:also-load lib-org-embark)
-        (add-to-list 'embark-keymap-alist '(org-roam-node . embark-org-roam-map))))))
+    (:after embark
+            (:also-load lib-org-embark)
+            (add-to-list 'embark-keymap-alist '(org-roam-node . embark-org-roam-map)))))
 
 (setup bibtex
   (:load-after org)
