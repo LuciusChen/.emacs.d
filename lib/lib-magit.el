49c9003c10eddbde52c572afb13ffebd39cb33a9
Lucius <chenyh572@gmai.com>
2025-02-28 11:23:04 +0800

    fix(lib): require gptel in gptel-commit function to prevent errors

--- lib/lib-magit.el
+++ lib/lib-magit.el
@@ -47,6 +47,7 @@ A scope may be provided to a commit's type, to provide additional contextual inf
 (defun gptel-commit ()
   "Generate commit message with gptel and insert it into the buffer."
   (interactive)
+  (require 'gptel)
   (let* ((lines (magit-git-lines "diff" "--cached"))
          (changes (string-join lines "\n")))
     (gptel-request changes :system gptel-commit-prompt)))
