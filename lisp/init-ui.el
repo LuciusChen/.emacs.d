9673352d5c19efb0fe1acbcd1d8b7f1cc6b05488
Lucius <chenyh572@gmai.com>
2025-03-06 03:43:17 +0800

    refactor(env): remove init-env.el and update related logic
    The init-env.el file has been removed, and related environment variable loading logic has been updated. Additionally, changes have been made to the init-ui.el and init-vc.el files to update font settings and authentication checks, respectively.

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -65,7 +65,7 @@
     (:also-load lib-face)
     (:hooks window-setup-hook +setup-fonts
             server-after-make-frame-hook +setup-fonts)
-    (:with-mode vterm-mode (:set-font *term-default-font*))
+    (:with-mode (vterm-mode eshell-mode) (:set-font *term-default-font*))
     (:with-mode (latex-mode prog-mode nxml-mode) (:set-font *prog-font*))
     (:with-mode org-mode (:set-font *org-font*))
     (:advice face-at-point :around #'+suggest-other-faces)))
