9763fc24b62612a4df4586a51645cdc5ed4ebcdc
Lucius <chenyh572@gmai.com>
2025-03-26 10:07:44 +0800

    feat: add Magit automation script and update README
    - Updated README with new section for Magit integration in IDEA
    - Added new shell script `magit.sh` to automate git operations with Magit in Emacs
    - Included new asset `idea-magit.jpg` for visual guidance in README

---
+++ sh/magit.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -o errexit
+
+# Get the root of the git repository
+git_root=$(git rev-parse --show-toplevel)
+
+# Open magit-status in Emacs
+emacsclient -e "(magit-status \"${git_root}\")" > /dev/null
+
+# Bring Emacs frame to the foreground (macOS specific)
+osascript << EOF
+tell application "System Events"
+        tell application process "Emacs"
+                set frontmost to true
+        end tell
+end tell
+EOF
+
+# Pass any additional arguments to emacsclient
+/Applications/Emacs.app/Contents/MacOS/bin/emacsclient "$@"
