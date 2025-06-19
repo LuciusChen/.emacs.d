ef58d5764e29e27c45f8fe5eefba46044cc4a8ce
lucius <chenyh572@gmail.com>
2025-06-19 17:38:45 +0800

    fix(debuger.sh): add comments for handling p2 metadata repository error

--- sh/debuger.sh
+++ sh/debuger.sh
@@ -17,4 +17,6 @@ cd ~/.emacs.d/debug-adapters
 git clone https://github.com/microsoft/java-debug.git
 cd java-debug
 # add `-U`' to force update.
+# [ERROR] Failed to resolve target definition file:/home/lucius/.emacs.d/debug-adapters/java-debug/com.microsoft.java.debug.target/com.microsoft.java.debug.tp.target: Failed to load p2 metadata repository from location https://download.eclipse.org/eclipse/updates/4.36-I-builds/: No repository found at https://download.eclipse.org/eclipse/updates/4.36-I-builds.
+# 出现以上错误的时候可以直接查找对应的地址在浏览器是否可以找到对应的 repository，可以尝试删除 I-builds。
 ./mvnw clean install
