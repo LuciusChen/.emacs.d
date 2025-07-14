3f67facfe70d741b7c987f9738d7d0fdee20e822
lucius <chenyh572@gmail.com>
2025-07-14 12:12:37 +0800

    feat(arch): add build script for Emacs PGTK with IGC and reorganize scripts directory

---
+++ scripts/arch/build-emacs-pgtk.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Define paths
+EMACS_D="$HOME/.emacs.d/scripts/arch/emacs-pgtk-igc"
+BUILD_DIR="$HOME/emacs-pgtk-igc"
+
+# Create the build directory if it doesn't exist
+mkdir -p "$BUILD_DIR"
+
+# Copy PKGBUILD to the build directory
+cp "$EMACS_D/PKGBUILD" "$BUILD_DIR/"
+
+# Change to the build directory
+cd "$BUILD_DIR"
+
+# Run makepkg to build and install the package
+makepkg -si
