8aa390dc553fe14ac1bda6be886a3a4269930e91
lucius <chenyh572@gmail.com>
2026-01-19 13:46:42 +0800

    update for consult's breaking changes

--- lib/lib-consult.el
+++ lib/lib-consult.el
@@ -59,7 +59,7 @@ off the default preview behavior in Consult for those commands."
  consult-ripgrep
  consult-git-grep consult-grep
  consult-bookmark consult-recent-file consult-xref
- consult--source-recent-file consult--source-project-recent-file consult--source-bookmark)
+ consult-source-recent-file consult-source-project-recent-file consult-source-bookmark)
 
 ;;;; provide
 (provide 'lib-consult)
