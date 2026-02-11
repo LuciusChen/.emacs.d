5aafcfc8895701fff479e8f324e60b0d98682c34
Lucius <chenyh572@gmai.com>
2026-02-11 09:16:37 +0800

    refactor(init-completion): simplify pinyinlib regex handling and remove size limit workaround

--- lisp/init-completion.el
+++ lisp/init-completion.el
@@ -23,35 +23,13 @@
                    orderless+basic-all
                    "Unholy mix of Orderless and Basic."))))
 
-;; pinyinlib-build-regexp-string expands every ASCII letter into a
-;; character class of all Chinese characters sharing that pinyin initial
-;; (e.g. "D" -> "[大打达...]").  The Emacs regex engine enforces
-;; MAX_BUF_SIZE (1<<15 = 32768 bytes) in regex-emacs.c, so long inputs
-;; easily overflow.  Instead of limiting input length, cap the generated
-;; regex at the byte level.
-;; https://github.com/emacs-mirror/emacs/blob/master/src/regex-emacs.c#L832
 (setup pinyinlib
   (:load-after orderless)
   (:when-loaded
-    (defvar pinyinlib-regex-max-size 3000)
-
-    (define-advice pinyinlib-build-regexp-string
-        (:override (str &rest args) limit-size)
-      (let ((result ""))
-        (catch 'stop
-          (mapc (lambda (c)
-                  (let ((piece (apply #'pinyinlib-build-regexp-char c args)))
-                    (when (> (+ (string-bytes result) (string-bytes piece))
-                             pinyinlib-regex-max-size)
-                      (throw 'stop nil))
-                    (setq result (concat result piece))))
-                str))
-        result))
-
     (add-to-list 'orderless-matching-styles
                  (lambda (str)
                    (orderless-regexp
-                    (pinyinlib-build-regexp-string str))))))
+                    (pinyinlib-build-regexp-string str nil nil t))))))
 
 (setup corfu
   (:defer (:require corfu))
